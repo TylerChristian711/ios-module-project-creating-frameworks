62315862bd5ec878d8e4d9ae72a05c76dca64886
Tyler Christian <tylerc71197@gmail.com>
2020-01-29 16:06:30 -0500

    Added loadingUITest framework to project

---
+++ LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingUI.h
@@ -0,0 +1,19 @@
+//
+//  LoadingUI.h
+//  LoadingUI
+//
+//  Created by Lambda_School_Loaner_218 on 1/29/20.
+//  Copyright © 2020 Lambda_School_Loaner_218. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUI.
+FOUNDATION_EXPORT double LoadingUIVersionNumber;
+
+//! Project version string for LoadingUI.
+FOUNDATION_EXPORT const unsigned char LoadingUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUI/PublicHeader.h>
+
+
